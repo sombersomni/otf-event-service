d09ea4bdff4a5612ba3f1be060158f8b1f710424
somberSomni <kartunehustla@gmail.com>
2023-05-24 23:41:30 -0400

    create first migration for task and tasktype

--- init_app.py
+++ init_app.py
@@ -8,7 +8,7 @@ from flask_migrate import Migrate
 # Load environment variables from .env file
 load_dotenv()
 
-DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+DATABASE_URI = os.getenv('TIMESCALE_DATABASE_URI')
 # contants
 app = Flask(__name__)
 r = redis.Redis(host='localhost', port=6379)
--- src/models/task.py
+++ src/models/task.py
@@ -1,13 +1,14 @@
 from init_app import db
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
+import uuid
 
 class TaskType(db.Model):
     id = Column(Integer, primary_key=True)
-    opaque_id = Column(UUID(as_uuid=True), index=True, nullable=False)
+    opaque_id = Column(UUID(as_uuid=True), default=uuid.uuid4, index=True, nullable=False)
     name = Column(db.String(100), index=True, nullable=False, unique=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.utcnow(), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
     created_by = Column(db.String(50), index=True, nullable=False)
 
     def __init__(self, name, created_by):
@@ -17,8 +18,8 @@ class TaskType(db.Model):
 class Task(db.Model):
     id = Column(Integer, primary_key=True)
     opaque_id = Column(UUID(as_uuid=True), index=True, nullable=False)
-    type = Column(String(100), index=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.utcnow(), nullable=False)
+    type = Column(String(100), default=uuid.uuid4, index=True, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
     created_by = Column(String(50), index=True, nullable=False)
     status = Column(String(20), default='scheduled', index=True, nullable=False)
 
